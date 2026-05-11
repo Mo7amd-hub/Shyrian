0a2e02c8367acb7d239169e951051e92ab70af98
hamoo <hamoody.x43@gmail.com>
2026-05-11 19:42:18 +0300

    created BloodRequestController

---
+++ Shyrian_project/Controllers/BloodRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shyrian_project.Controllers
+{
+	public class BloodRequestController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
